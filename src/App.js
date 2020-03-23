eedc58d184163ad489b19a01656528f4a7da9634
Lloyd van Zaalen <lfb0801@github.com>
2020-03-23 09:59:46 +0100

    added a second example page to prove router = working

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {
   Link
 } from "react-router-dom";
 import Home from './components/Home'
+import Contact from './components/Contact'
 
 function App() {
   return (
@@ -16,20 +17,18 @@ function App() {
           <li>
             <Link to="/">Home</Link>
           </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
         </ul>
 
         <hr />
-
-        {/*
-          A <Switch> looks through all its children <Route>
-          elements and renders the first one whose path
-          matches the current URL. Use a <Switch> any time
-          you have multiple routes, but you want only one
-          of them to render at a time
-        */}
         <Switch>
           <Route exact path="/">
             <Home />
+          </Route>
+          <Route exact path="/contact">
+            <Contact />
           </Route>
         </Switch>
       </div>
