ee7c474c04a3f18afdfe60ede4a8c7aa548bb95c
exZell11 <60896251+exZell11@users.noreply.github.com>
2020-04-21 15:47:32 +0200

    Toolbar and Sidebar
    created toolbar and retractable sidebar for webpage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,60 @@
-import React from 'react';
-import './App.css';
-import {
+
+/*import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link
 } from "react-router-dom";
-import Home from './components/Home'
-import Contact from './components/Contact'
+import Home from './Components/Home'
+import Contact from './Components/Contact'*/
 
+import React, {Component} from 'react';
+import './App.css';
+import Sidebar from './Components/Toolbar/Toolbar';
+import Drawer from './Components/Drawer/Drawer';
+import Backdrop from './Components/Backdrop/Backdrop';
+
+
+class App extends Component {
+  state = {
+    drawerOpen: false
+  };
+
+
+  drawerToggleClickHandler = () => {
+    this.setState((prevState) => {
+      return {drawerOpen: !prevState.drawerOpen}
+    });
+  };
+
+  backdropClickHandler = () => {
+    this.setState({drawerOpen:false});
+  };
+
+  render(){
+
+    let backdrop;
+
+    if (this.state.drawerOpen){
+      backdrop =  <Backdrop click = {this.backdropClickHandler}/>
+    }
+    return (
+        <div style={{height: '100%'}}>
+          <Sidebar drawerClickHandler = {this.drawerToggleClickHandler} />
+          <Drawer show = {this.state.drawerOpen}/>
+          {backdrop}
+          <main style={{marginTop: '64px'}}>
+            <p>This is the page content</p></main>
+
+        </div>
+    );
+  }
+}
+
+export default App;
+
+
+/*
 function App() {
   return (
     <Router>
@@ -31,9 +77,11 @@ function App() {
             <Contact />
           </Route>
         </Switch>
+
       </div>
     </Router>
   );
 }
 
 export default App;
+*/
