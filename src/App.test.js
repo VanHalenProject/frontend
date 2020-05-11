d9f3070a2f33277f6b40823cc006a36ac3ec55d2
Lloyd van Zaalen <lfb0801@github.com>
2020-05-11 18:48:14 +0200

    Update App.test.js

---
+++ src/App.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
+
+test('dumb test', () => {
+  // The string:
+  var str = "Hello world!";
+
+  // Look for "Hello"
+  var patt = /Hello/g;
+  var result = patt.test(str);
+
+  // Look for "W3Schools"
+  patt2 = /W3Schools/g;
+  result2 = patt2.test(str);
+});
