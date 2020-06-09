3276bfa641d338a11a356272efabf73cbc57affe
RonaldVanDenBurg <60896718+RonaldVanDenBurg@users.noreply.github.com>
2020-06-09 21:16:32 +0200

    Create App2.js

---
+++ src/components/App2.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "./App2.css";
+import Home from "./components/Home";
+import CreateAccount from "./components/CreateAccount";
+import Login from "./components/Login";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <ul>
+          <li>
+            <Link className="link" to="/">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link className="link" to="/login">
+              Login
+            </Link>
+          </li>
+          <li>
+            <Link className="link" to="/createAccount">
+              Create account
+            </Link>
+          </li>
+        </ul>
+
+        <hr />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+          <Route exact path="/createAccount">
+            <CreateAccount />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
