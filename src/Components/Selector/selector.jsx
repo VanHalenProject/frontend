f5d1fddc2087d5059199021f943f0c965369637b
Nieuwburg,Patrick P.E.J. van <p.vannieuwburg@student.fontys.nl>
2020-06-09 21:50:51 +0200

    Refactor Skittleselector

--- src/Components/Selector/selector.jsx
+++ src/Components/Selector/selector.jsx
@@ -1,24 +1,56 @@
 import React, { Component } from "react";
 export default class Selector extends Component {
 	state = {
+		amountSelected: 0,
 		value: this.props.selector.value,
 	};
 
 	handleIncrement = () => {
-		this.setState({ value: this.state.value + 1 });
+		this.setState(
+			(prevState) => {
+				return { amountSelected: prevState.amountSelected + 1 };
+			},
+			() => this.props.onIncrement(this.props.selector)
+		);
 	};
 
 	handleDecrement = () => {
-		if (this.state.value === 0) {
-			this.setState({ value: this.state.value - 1 });
-		}
+		this.setState(
+			(prevState) => {
+				return { amountSelected: prevState.amountSelected - 1 };
+			},
+			() => this.props.onDecrement(this.props.selector)
+		);
+	};
+
+	handleReset = () => {
+		this.setState(
+			(prevState) => {
+				return { amountSelected: 0 };
+			},
+			() => this.props.onReset(this.props.selector)
+		);
 	};
 
 	render() {
 		return (
 			<div>
 				{this.props.children}
-				<span className={this.getBadge()}>{this.state.value}</span>
+				<span className="badge m-2 badge-warning">
+					{this.state.amountSelected}
+				</span>
+				{/* <button
+					onClick={() => this.props.onIncrement(this.props.selector)}
+					className="btn btn-secondary btn-sm m-2"
+				>
+					+
+				</button>
+				<button
+					onClick={() => this.props.onDecrement(this.props.selector)}
+					className="btn btn-secondary btn-sm m-2"
+				>
+					-
+				</button> */}
 				<button
 					onClick={this.handleIncrement}
 					className="btn btn-secondary btn-sm m-2"
@@ -31,12 +63,14 @@ export default class Selector extends Component {
 				>
 					-
 				</button>
+				{/* <button
+					onClick={() => this.props.onReset(this.props.selector)}
+					className="btn btn-danger btn-sm m-2"
+				>
+					Reset
+				</button> */}
 				<button
-					onClick={() =>
-						this.setState({
-							value: 0,
-						})
-					}
+					onClick={this.handleReset}
 					className="btn btn-danger btn-sm m-2"
 				>
 					Reset
@@ -44,17 +78,11 @@ export default class Selector extends Component {
 			</div>
 		);
 	}
-
-	getBadge() {
-		let classes = "badge m-2 badge-";
-		classes += this.state.value === 0 ? "warning" : "primary";
-		return classes;
-	}
 }
 
 // changeRed =() => {
 // 	const redSelector = this.state.skittleSelectors.findIndex(element => element.color === "Red");
-// 	let changeRed = [...this.state.skittleSelectors];
+// 	changeRed[redSelector] = {...changeRed[redSelector], availability: result.data.Red}
 // 	axios.get(
 // 		"https://0918fe64-9b80-413e-b721-6f8d296f9ff3.mock.pstmn.io?Green=12&Red=15&Yellow=10&Purple=2&Orange=8"
 // 	).then((result) => {
@@ -71,3 +99,9 @@ export default class Selector extends Component {
 // 	changeRed();
 
 // };
+
+// onClick={() =>
+// 	this.setState({
+// 		value: 0,
+// 	})
+// }
--- src/Components/Selector/selectors.jsx
+++ src/Components/Selector/selectors.jsx
@@ -13,6 +13,35 @@ export default class Selectors extends Component {
 		],
 	};
 
+	handleIncrement = (selector) => {
+		const selectors = [...this.state.skittleSelectors];
+		const index = selectors.indexOf(selector);
+		if(selectors[index].value < selectors[index].availability){
+			selectors[index].value++;
+		} else {
+			alert("No more skittles left")
+		}
+		
+		console.log(selector);
+	};
+
+	handleDecrement = (selector) => {
+		const selectors = [...this.state.skittleSelectors];
+		const index = selectors.indexOf(selector);
+		if(selectors[index].value > 0){
+			selectors[index].value--;	
+		} else{
+			alert("Amount can not be less then 0")
+		}
+		console.log(selector);
+	};
+
+	handleReset = (selector) => {
+		const selectors = [...this.state.skittleSelectors];
+		const index = selectors.indexOf(selector);
+		selectors[index].value = 0;
+	};
+
 	// getSkittleAvailibility = () => {
 	// 	axios
 	// 		.get(
@@ -56,25 +85,62 @@ export default class Selectors extends Component {
 	// 		});
 	// };
 
-	// handleReset = () => {
-	// 	const selectors = this.state.skittleSelectors.map((c) => {
-	// 		console.log(c.value);
-	// 		c.value = 0;
-	
-	// 	});
-	// 	this.setState({ selectors });
-	// };
+	postData = () => {
+		const greenSelector = this.state.skittleSelectors.findIndex(
+			(element) => element.color === "Green"
+		);
+		const redSelector = this.state.skittleSelectors.findIndex(
+			(element) => element.color === "Red"
+		);
+		const yellowSelector = this.state.skittleSelectors.findIndex(
+			(element) => element.color === "Yellow"
+		);
+		const purpleSelector = this.state.skittleSelectors.findIndex(
+			(element) => element.color === "Purple"
+		);
+		const orangeSelector = this.state.skittleSelectors.findIndex(
+			(element) => element.color === "Orange"
+		);
+		
+		let greenValue = this.state.skittleSelectors[greenSelector].value;
+		let redValue = this.state.skittleSelectors[redSelector].value;
+		let yellowValue = this.state.skittleSelectors[yellowSelector].value;
+		let purpleValue = this.state.skittleSelectors[purpleSelector].value;
+		let orangeValue = this.state.skittleSelectors[orangeSelector].value;
+		
+		let sendData = JSON.stringify({
+			Green: greenValue,
+			Red: redValue,
+			Yellow: yellowValue,
+			Purple: purpleValue,
+			Orange: orangeValue
+		})
+		
+		const requestOptions = {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: sendData
+		};
+		fetch('https://0918fe64-9b80-413e-b721-6f8d296f9ff3.mock.pstmn.io?Green&Red&Yellow&Purple&Orange', requestOptions)
+			.then(response => response.json())
+			console.log(sendData);
+	};
+
 
 	render() {
 		return (
 			<div>
 				<button onClick={this.getSkittleAvailibility}>GETDATA</button>
-				<button>POSTDATA</button>
+				<button onClick={this.postData}>POSTDATA</button>
 				{this.state.skittleSelectors.map((selector) => (
 					<Selector
+						value={this.state.value}
 						key={selector.id}
 						selector={selector}
+						onIncrement={this.handleIncrement}
+						onDecrement={this.handleDecrement}
 						onReset={this.handleReset}
+						onChangedValue={this.onChangedValue}
 					>
 						<p>{selector.color}:</p>
 						<p>amount availible: {selector.availability}</p>
