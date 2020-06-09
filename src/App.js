f15aeff51cf0ca6abc22348f0bf0a00c833e72ec
RonaldVanDenBurg <60896718+RonaldVanDenBurg@users.noreply.github.com>
2020-06-09 23:07:50 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,41 +1,42 @@
 import React, { Component } from "react";
 import "./App.css";
+import "./css/Login.css";
 import Sidebar from "./Components/Toolbar/Toolbar";
 import Drawer from "./Components/Drawer/Drawer";
 import Backdrop from "./Components/Backdrop/Backdrop";
 
 class App extends Component {
-	state = {
-		drawerOpen: false,
-	};
+  state = {
+    drawerOpen: false,
+  };
 
-	drawerToggleClickHandler = () => {
-		this.setState((prevState) => {
-			return { drawerOpen: !prevState.drawerOpen };
-		});
-	};
+  drawerToggleClickHandler = () => {
+    this.setState((prevState) => {
+      return { drawerOpen: !prevState.drawerOpen };
+    });
+  };
 
-	backdropClickHandler = () => {
-		this.setState({ drawerOpen: false });
-	};
+  backdropClickHandler = () => {
+    this.setState({ drawerOpen: false });
+  };
 
-	render() {
-		let backdrop;
+  render() {
+    let backdrop;
 
-		if (this.state.drawerOpen) {
-			backdrop = <Backdrop click={this.backdropClickHandler} />;
-		}
-		return (
-			<div style={{ height: "100%" }}>
-				<Sidebar drawerClickHandler={this.drawerToggleClickHandler} />
-				<Drawer show={this.state.drawerOpen} />
-				{backdrop}
-				<main style={{ marginTop: "64px" }}>
-					<p>This is the page content</p>
-				</main>
-			</div>
-		);
-	}
+    if (this.state.drawerOpen) {
+      backdrop = <Backdrop click={this.backdropClickHandler} />;
+    }
+    return (
+      <div style={{ height: "100%" }}>
+        <Sidebar drawerClickHandler={this.drawerToggleClickHandler} />
+        <Drawer show={this.state.drawerOpen} />
+        {backdrop}
+        <main style={{ marginTop: "64px" }}>
+          <p>This is the page content</p>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
